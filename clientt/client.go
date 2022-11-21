bf39af941c9ee8562d4f19cac9af05b75b2263ee
Zalupessa <little.mikle@yahoo.com>
2022-11-21 16:33:10 +0300

    working

---
+++ clientt/client.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	pb "github.com/LittleMikle/grpc_go.git/gen/proto"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:8081", grpc.WithInsecure())
+	if err != nil {
+		log.Println(err)
+	}
+	client := pb.NewTestApiClient(conn)
+
+	resp, err := client.Echo(context.Background(), &pb.ResponseRequest{Msg: "Hello"})
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(resp)
+	fmt.Println(resp.Msg)
+}
